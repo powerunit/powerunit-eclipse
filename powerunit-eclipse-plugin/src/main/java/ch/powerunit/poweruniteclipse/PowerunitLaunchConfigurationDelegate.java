45d4bfb01a9bb669b8a3bf9de2a8c352b91f6e62
boretti <mathieu.boretti@gmail.com>
2015-07-27 19:56:57 +0200

    #12 Change the detection of class
    The goal is to ignore the once annotated with @TestDelegator

--- powerunit-eclipse-plugin/src/main/java/ch/powerunit/poweruniteclipse/PowerunitLaunchConfigurationDelegate.java
+++ powerunit-eclipse-plugin/src/main/java/ch/powerunit/poweruniteclipse/PowerunitLaunchConfigurationDelegate.java
@@ -29,6 +29,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
@@ -43,20 +44,18 @@ import ch.powerunit.poweruniteclipse.helper.TestClassSearch;
  * @author borettim
  *
  */
-public class PowerunitLaunchConfigurationDelegate extends
-		AbstractJavaLaunchConfigurationDelegate {
+public class PowerunitLaunchConfigurationDelegate extends AbstractJavaLaunchConfigurationDelegate {
 
 	public static final String CH_POWERUNIT_POWER_UNIT_MAIN_RUNNER = "ch.powerunit.PowerUnitMainRunner"; //$NON-NLS-1$
 
-	public static final String PACKAGE_FRAGMENT_NAME = PowerunitLaunchConfigurationDelegate.class
-			+ ".packagefragment";
+	public static final String PACKAGE_FRAGMENT_NAME = PowerunitLaunchConfigurationDelegate.class + ".packagefragment";
 
 	public static final String PACKAGE_FRAGMENT_ROOT_NAME = PowerunitLaunchConfigurationDelegate.class
 			+ ".packagefragmentRoot";
 
 	@Override
-	public void launch(ILaunchConfiguration configuration, String mode,
-			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
 		IJavaProject project = verifyJavaProject(configuration);
 
 		IVMInstall vm = verifyVMInstall(configuration);
@@ -78,37 +77,30 @@ public class PowerunitLaunchConfigurationDelegate extends
 		String classpath[] = getClasspath(configuration);
 
 		String mainTypes = getMainTypeName(configuration);
-		String packageFragment = configuration.getAttribute(
-				PACKAGE_FRAGMENT_NAME, "");
-		String packageFragmentRoot = configuration.getAttribute(
-				PACKAGE_FRAGMENT_ROOT_NAME, "");
+		String packageFragment = configuration.getAttribute(PACKAGE_FRAGMENT_NAME, "");
+		String packageFragmentRoot = configuration.getAttribute(PACKAGE_FRAGMENT_ROOT_NAME, "");
 
 		if (mainTypes == null || "".equalsIgnoreCase(mainTypes.trim())) {
 			IType[] types = null;
 			try {
-				if (packageFragment != null
-						&& !"".equalsIgnoreCase(packageFragment.trim())) {
-					types = TestClassSearch
-							.searchTestSuiteClazzFromPackageFragment(
-									ProgressManager.getInstance(),
-									project.findPackageFragmentRoot(
-											project.getPath().append(
-													packageFragmentRoot))
-											.getPackageFragment(packageFragment));
-				} else if (packageFragmentRoot != null
-						&& !"".equalsIgnoreCase(packageFragmentRoot.trim())) {
-					types = TestClassSearch
-							.searchTestSuiteClazzFromPackageFragmentRoot(
-									ProgressManager.getInstance(),
-									project.findPackageFragmentRoot(project
-											.getPath().append(
-													packageFragmentRoot)));
+				if (packageFragment != null && !"".equalsIgnoreCase(packageFragment.trim())) {
+					types = TestClassSearch.searchTestSuiteClazzFromPackageFragment(ProgressManager.getInstance(),
+							project.findPackageFragmentRoot(project.getPath().append(packageFragmentRoot))
+									.getPackageFragment(packageFragment));
+				} else if (packageFragmentRoot != null && !"".equalsIgnoreCase(packageFragmentRoot.trim())) {
+					types = TestClassSearch.searchTestSuiteClazzFromPackageFragmentRoot(ProgressManager.getInstance(),
+							project.findPackageFragmentRoot(project.getPath().append(packageFragmentRoot)));
 				} else {
-					types = TestClassSearch.searchTestSuiteClazzFromProject(
-							ProgressManager.getInstance(), project);
+					types = TestClassSearch.searchTestSuiteClazzFromProject(ProgressManager.getInstance(), project);
 				}
 				StringBuilder sb = new StringBuilder();
-				for (IType t : types) {
+				type: for (IType t : types) {
+					for (IAnnotation a : t.getAnnotations()) {
+						if (a.getElementName().equals("ch.powerunit.TestDelegator")
+								|| a.getElementName().equals("TestDelegator")) {
+							continue type;
+						}
+					}
 					sb.append(t.getFullyQualifiedName()).append(",");
 				}
 				mainTypes = sb.toString().replaceAll(",$", "");
@@ -120,14 +112,11 @@ public class PowerunitLaunchConfigurationDelegate extends
 		}
 
 		// Create VM config
-		VMRunnerConfiguration runConfig = new VMRunnerConfiguration(
-				CH_POWERUNIT_POWER_UNIT_MAIN_RUNNER, classpath);
+		VMRunnerConfiguration runConfig = new VMRunnerConfiguration(CH_POWERUNIT_POWER_UNIT_MAIN_RUNNER, classpath);
 		runConfig.setEnvironment(getEnvironment(configuration));
-		runConfig.setProgramArguments(new String[] {
-				p.toFile().getAbsolutePath(), mainTypes });
+		runConfig.setProgramArguments(new String[] { p.toFile().getAbsolutePath(), mainTypes });
 		if (!"".equals(getVMArguments(configuration))) { //$NON-NLS-1$
-			runConfig
-					.setVMArguments(new String[] { getVMArguments(configuration) });
+			runConfig.setVMArguments(new String[] { getVMArguments(configuration) });
 		}
 		runConfig.setWorkingDirectory(workingDirName);
 
